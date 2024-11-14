776ed960e446bbab0a8c9e79a1122e08a9b621f1
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-11-14 09:55:50 +0900

    wordlist_screen.dart 삭제기능추가

--- lib/screen/wordlist_screen.dart
+++ lib/screen/wordlist_screen.dart
@@ -11,6 +11,40 @@ class WordListScreen extends StatefulWidget {
 }
 
 class _WordListScreenState extends State<WordListScreen> {
+  List<bool> _selectedWords = []; // 선택된 단어를 추적하는 리스트
+  bool _isDeletingMode = false; // 삭제 모드 상태 변수 추가
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedWords =
+        List<bool>.filled(widget.wordSet.words.length, false); // 초기화
+  }
+
+  void _toggleSelection(int index) {
+    setState(() {
+      _selectedWords[index] = !_selectedWords[index]; // 선택 상태 토글
+    });
+  }
+
+  void _deleteSelectedWords() {
+    setState(() {
+      if (widget.wordSet.words.isNotEmpty) {
+        // 리스트가 비어있지 않은지 확인
+        widget.wordSet.words.removeWhere((word) =>
+            _selectedWords[widget.wordSet.words.indexOf(word)]); // 선택된 단어 삭제
+        // 삭제 후 라벨 재정렬
+        for (int i = 0; i < widget.wordSet.words.length; i++) {
+          widget.wordSet.words[i].label = i + 1; // 라벨을 1부터 순서대로 재설정
+        }
+        _selectedWords =
+            List<bool>.filled(widget.wordSet.words.length, false); // 초기화
+      }
+      widget.wordSet.save(); // 변경 사항 저장
+      _isDeletingMode = false; // 삭제 모드 종료
+    });
+  }
+
   void _showAddWordDialog(BuildContext context) {
     final TextEditingController englishController = TextEditingController();
     final TextEditingController meaningController = TextEditingController();
@@ -79,12 +113,50 @@ class _WordListScreenState extends State<WordListScreen> {
         title: const Text('단어 목록'),
         centerTitle: true,
         actions: [
-          IconButton(
-            onPressed: () {
-              _showAddWordDialog(context);
-            },
-            icon: const Icon(Icons.add, size: 30),
-          ),
+          if (_isDeletingMode) // 삭제 모드일 때만 삭제 버튼 표시
+            TextButton(
+              onPressed: _deleteSelectedWords,
+              child: const Text('삭제'),
+            )
+          else // 삭제 모드가 아닐 때만 편집 아이콘 표시
+            IconButton(
+              onPressed: () {
+                // 팝업 메뉴를 보여줍니다.
+                showMenu(
+                  context: context,
+                  position: const RelativeRect.fromLTRB(
+                      100.0, 100.0, 0.0, 0.0), // 위치 조정
+                  items: [
+                    const PopupMenuItem(
+                      value: 'add',
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text('추가'),
+                      ),
+                    ),
+                    const PopupMenuItem(
+                      value: 'delete',
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text('삭제'),
+                      ),
+                    ),
+                  ],
+                ).then((value) {
+                  if (value == 'add') {
+                    _showAddWordDialog(context); // 추가 다이얼로그 호출
+                  } else if (value == 'delete') {
+                    setState(() {
+                      // 삭제 모드로 전환
+                      _isDeletingMode = true; // 삭제 모드 상태 변수 추가
+                      _selectedWords = List<bool>.filled(
+                          widget.wordSet.words.length, false); // 초기화
+                    });
+                  }
+                });
+              },
+              icon: const Icon(Icons.edit, size: 30),
+            ),
         ],
       ),
       body: SingleChildScrollView(
@@ -106,7 +178,7 @@ class _WordListScreenState extends State<WordListScreen> {
                     child: Padding(
                       padding: EdgeInsets.all(8.0),
                       child: Text(
-                        '라벨',
+                        '번호',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
@@ -156,7 +228,7 @@ class _WordListScreenState extends State<WordListScreen> {
               ],
             ),
             // 데이터 행
-            for (var word in widget.wordSet.words)
+            for (var index = 0; index < widget.wordSet.words.length; index++)
               TableRow(
                 children: [
                   TableCell(
@@ -164,8 +236,29 @@ class _WordListScreenState extends State<WordListScreen> {
                     child: Center(
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
+                        child: _isDeletingMode // 삭제 모드일 때 체크박스 표시
+                            ? Checkbox(
+                                value: _selectedWords[index],
+                                onChanged: (value) {
+                                  _toggleSelection(index); // 체크 상태 토글
+                                },
+                                visualDensity: const VisualDensity(
+                                    horizontal: -4, vertical: -4),
+                              )
+                            : Text(
+                                widget.wordSet.words[index].label.toString(),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                      ),
+                    ),
+                  ),
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          word.label.toString(),
+                          widget.wordSet.words[index].english,
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
@@ -177,19 +270,7 @@ class _WordListScreenState extends State<WordListScreen> {
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          word.english,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                  ),
-                  TableCell(
-                    verticalAlignment: TableCellVerticalAlignment.middle,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          word.meaning,
+                          widget.wordSet.words[index].meaning,
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
@@ -201,15 +282,20 @@ class _WordListScreenState extends State<WordListScreen> {
                       child: InkWell(
                         onTap: () {
                           setState(() {
-                            word.memorized = !word.memorized; // 상태 반전
+                            widget.wordSet.words[index].memorized =
+                                !widget.wordSet.words[index].memorized; // 상태 반전
                             widget.wordSet.save(); // 변경 사항 저장
                           });
                         },
                         child: Container(
                           padding: const EdgeInsets.all(14.0),
                           child: Icon(
-                            word.memorized ? Icons.check : Icons.close,
-                            color: word.memorized ? Colors.green : Colors.red,
+                            widget.wordSet.words[index].memorized
+                                ? Icons.check
+                                : Icons.close,
+                            color: widget.wordSet.words[index].memorized
+                                ? Colors.green
+                                : Colors.red,
                             size: 20,
                           ),
                         ),
